8eeaa3fb1c77a4227e2ea348feb11220dd938077
Jihed CHALGHAF <chalghaf.jihed@gmail.com>
2020-09-02 17:03:31 +0100

    integrated mongo atlas replica set

--- web_app/server/config/database.config.js
+++ web_app/server/config/database.config.js
@@ -3,5 +3,6 @@ var LOCAL_IP = ip.address();
 
 module.exports = {
     url: 'mongodb://localhost:27017/firstdb',
-    url_rs: `mongodb://${LOCAL_IP}:27011,${LOCAL_IP}:27012,${LOCAL_IP}:27013?replicaSet=rs`
+    //url_rs: `mongodb://${LOCAL_IP}:27011,${LOCAL_IP}:27012,${LOCAL_IP}:27013?replicaSet=rs`
+    url_rs: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@rs.7apfo.mongodb.net/EventsDB?retryWrites=true&w=majority`
 }
